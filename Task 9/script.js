7468c6c26a4246674fa60c94f7a11c538801ea0a
Paulius Naujalis <paulius.naujalis@codeacademylt.onmicrosoft.com>
2022-08-17 22:21:45 +0300

    --Task 09-- Done!

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,25 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+class Movie {
+    constructor(title, director, budget) {
+        this.movie = title;
+        this.direct = director;
+        this.amount = wasExpensive(+budget)
+
+    }
+}
+
+function wasExpensive(amount) {
+    if (amount > 100000000) {
+        return true
+    }
+    if (amount < 100000000) {
+        return false
+    }
+}
+
+const thunderball = new Movie("Thunderball: 007", "Terence Young", 141000000);
+
+console.log('exampleCall --->', thunderball);
+
