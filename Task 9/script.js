35cf4e370448411889194ecb7b352f51066c066b
Paulius Naujalis <paulius.naujalis@codeacademylt.onmicrosoft.com>
2022-08-19 11:28:19 +0300

    --Task 09-- Done!

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,25 +9,43 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+
+// Solution 1
+// _______________________________________________________________________________________________
+
+// class Movie {
+//     constructor(title, director, budget) {
+//         this.movie = title;
+//         this.direct = director;
+//         this.amount = +budget;
+//     }
+//     wasExpensive() {
+
+//         return (this.amount > 100000000 ? true : false)
+//     }
+// }
+
+// const thunderball = new Movie("Thunderball: 007", "Terence Young", 141000000)
+// console.log('Is it expensive? ', thunderball.wasExpensive());
+
+// _______________________________________________________________________________________________
+
+
+// Solution 2
+// _______________________________________________________________________________________________
 class Movie {
     constructor(title, director, budget) {
         this.movie = title;
         this.direct = director;
-        this.amount = wasExpensive(+budget)
-
+        this.amount = wasExpensive(+budget);
     }
 }
 
-function wasExpensive(amount) {
-    if (amount > 100000000) {
-        return true
-    }
-    if (amount < 100000000) {
-        return false
-    }
+function wasExpensive(amt) {
+    return (amt > 100000000 ? true : false)
 }
+const thunderball = new Movie("Thunderball: 007", "Terence Young", 141000000)
+console.log('Thunderball: 007 ', thunderball);
 
-const thunderball = new Movie("Thunderball: 007", "Terence Young", 141000000);
-
-console.log('exampleCall --->', thunderball);
-
+// _______________________________________________________________________________________________
